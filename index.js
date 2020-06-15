33e9433aa489f896e61b295f5377e80a1f607a76
Cedric Patton <ceddyp117@gmail.com>
2020-06-15 23:57:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPLay(){
+  
+}
+
+function johnLennonFacts(){
+  
+}
+
+var num = 15;
+
+
+function iLoveTheBeatles(num) {
+  
+  var emptyArray  = ["I love the Beatles!"];
+
+do{
+ emptyArray.push(["I love the Beatles!")
+}while(num < 15);
+
+}
